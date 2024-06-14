cb82014e717bcc9898ae223ff86e1159c6eb2b56
Asmeg <repulsive7@gmail.com>
2024-06-14 15:09:48 +0900

    20240614

--- face-recognition-frontend/vite.config.ts
+++ face-recognition-frontend/vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        outDir: 'build', // 确保输出目录为 build
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'), // 指定 index.html 文件的位置
-            },
-        },
-    },
     server: {
         proxy: {
             '/api': {
@@ -20,5 +14,8 @@ export default defineConfig({
                 secure: false,
             },
         },
+    },
+    build: {
+        outDir: 'build',
     },
 });
