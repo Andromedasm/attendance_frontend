8bb36b1105e72d037522e60c45bd6a400cc23005
Asmeg <repulsive7@gmail.com>
2024-06-14 14:34:44 +0900

    20240614
    202020

--- face-recognition-frontend/vite.config.ts
+++ face-recognition-frontend/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: [
-      'axios',
-      '@fortawesome/react-fontawesome',
-      '@fortawesome/fontawesome-free/css/all.min.css'
-    ],
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: [
-        '@fortawesome/fontawesome-free/css/all.min.css'
-      ],
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0', // 使得可以通过网络访问
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'https://insightface.japaneast.cloudapp.azure.com:5000',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-  },
+    build: {
+        outDir: 'build',
+    },
 });
